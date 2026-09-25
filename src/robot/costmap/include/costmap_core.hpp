d93857f096aa9028b2a5c432ae0c7b41dee22e2b
lemonlime4 <71093251+lemonlime4@users.noreply.github.com>
2026-09-25 16:36:05 -0400

    increase costmap inflation radius

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -8,11 +8,12 @@
 namespace robot
 {
 
-class CostmapCore {
+  class CostmapCore
+  {
   public:
     // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
 
-    explicit CostmapCore(const rclcpp::Logger& logger);
+    explicit CostmapCore(const rclcpp::Logger &logger);
     void initializeCostmap();
     void convertToGrid(double range, double angle, int &x_grid, int &y_grid);
     void markObstacle(int x_grid, int y_grid);
@@ -21,21 +22,18 @@ class CostmapCore {
 
   private:
     rclcpp::Logger logger_;
-    
+
     // all the used variables for the costmap
     std::vector<std::vector<int>> grid_;
 
     // the grid is 300 cells tall and wide, with each cell being 0.1m in real space
-    int height_ {300};
-    int width_ {300};
-    double resolution_ {0.1};
+    int height_{300};
+    int width_{300};
+    double resolution_{0.1};
 
-    
-    double inflation_radius_ {1.0};  // obstacles inflated by 1.0m
-    
+    double inflation_radius_{2.0}; // obstacles inflated by 1.0m
+  };
 
-};
+}
 
-}  
-
-#endif  
+#endif
